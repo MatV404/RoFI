9550607cfe4f2331ac2cbc6788dfcac123098eaa
Matej Vavrek <matvav13@gmail.com>
2023-05-07 00:33:41 +0200

    fix: removed unecessary stuff from the packet filter

--- examples/simulator/election/packet_filter.py
+++ examples/simulator/election/packet_filter.py
@@ -9,10 +9,6 @@ counted_addrs = [
     b'\xff\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xea\x00\xea'
 ]
 
-routing_addrs = [
-    b'\xff\x02\x00\x00\x00\x00\x00\x00\x00\n\x00\x0b\x00\x0b\x00\xae'
-]
-
 total_counter: int = 0
 
 def filter( packet, sender, receiver ):
@@ -22,10 +18,4 @@ def filter( packet, sender, receiver ):
     if pack_array[24:40] in counted_addrs:
         total_counter += 1
         print(total_counter)
-        print(f"sender: {sender.module_id}, connector: { sender.connector_idx }")
-        print(f"receiver: {receiver.module_id}, connector: { receiver.connector_idx }")
-
-    if pack_array[24:40] in routing_addrs:
-        print(f"Routing Message -> sender: {sender.module_id}, connector: { sender.connector_idx }")
-        print(f"Routing Message -> receiver: {receiver.module_id}, connector: { receiver.connector_idx }")
     return packet, 0
